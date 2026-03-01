de11433da762975daaa146d2ed0cd060fcc93a13
belmontprogramador <belmontprogramador@gmail.com>
2026-03-01 19:07:27 -0300

    versão build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // 🔥 Ativa o build estático
-  
   images: {
     remotePatterns: [
       {
@@ -12,20 +10,15 @@ const nextConfig: NextConfig = {
         port: '3001',
         pathname: '/uploads/**',
       },
-      // 🔥 Adiciona o padrão para produção
-      {
-        protocol: 'https',
-        hostname: 'api.radiocupom.online',
-        port: '',
-        pathname: '/uploads/**',
-      },
+      // Adicione outros hosts conforme necessário (ex: em produção)
+      // {
+      //   protocol: 'https',
+      //   hostname: 'api.seudominio.com',
+      //   port: '',
+      //   pathname: '/uploads/**',
+      // },
     ],
-    // ⚠️ Necessário para imagens em build estático
-    unoptimized: true,
   },
-  
-  // Opcional: desabilita a geração de etags se necessário
-  // generateEtags: false,
 };
 
 export default nextConfig;
